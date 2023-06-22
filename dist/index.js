dd556b69ff09cf5c0ab31b06d84513a0fda9fc41
cpun2 <cpun@andrew.cmu.edu>
2023-06-22 10:17:02 -0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.CortexAPI = void 0;
+var api_1 = require("./api");
+Object.defineProperty(exports, "CortexAPI", { enumerable: true, get: function () { return api_1.CortexAPI; } });
