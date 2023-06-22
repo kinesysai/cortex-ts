db87e3d86de73fccb9014982527560b57b66dae5
cpun2 <cpun@andrew.cmu.edu>
2023-06-21 17:55:38 -0700

    getDocument function

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,29 @@
-import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
+import axios, { AxiosResponse, AxiosRequestConfig, AxiosPromise } from 'axios';
 
+export interface Document {
+  data_source_id: string;
+  created: number;
+  document_id: string;
+  timestamp: number;
+  tags: string[];
+  hash: string;
+  text_size: number;
+  chunk_count: number;
+  chunks: {
+    text: string;
+    hash: string;
+    offset: number;
+    vector?: number[] | null;
+    score?: number | null;
+  }[];
+  text?: string | null;
+  source_url?: string | null;
+};
+
+const createRequestFunction = function (config: AxiosRequestConfig, endpoint: string , BASE_PATH: string) {
+    const axiosRequestArgs = {...config, url: BASE_PATH + endpoint};
+    return axios.request(axiosRequestArgs);
+};
 
 /**
  *
@@ -9,6 +33,7 @@ import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 export class CortexAPI {
     protected apiKey: string;
     protected userId: string;
+    protected basePath: string;
 
     constructor(apiKey?: string, userId?: string) {
         if (apiKey) {
@@ -16,14 +41,26 @@ export class CortexAPI {
         }
         if (userId) {
             this.userId = userId;
+            this.basePath = 'https://trycortex.ai/api/sdk/p/'+ userId;
         }
     }
-    public getDocument() {
-    
+
+    public getDocument(knowledgeName:string, documentID:string): AxiosPromise<Document> {
+        const config = {
+            headers: {
+              'Authorization': `Bearer ${this.apiKey}`,
+              'Content-Type': 'application/json'
+            },
+            method: 'GET',
+          };
+        const endpoint = '/knowledge/'+ knowledgeName + '/d/' + documentID;
+        return createRequestFunction(config, endpoint, this.basePath);
     }
+
     public uploadDocument() {
     
     }
+
     public deleteDocument() {
     
     }
